eee65ce6a9d4500eefc10d0058af9fd9137f37b8
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-04-06 16:41:31 +0000

    Add product detail page and resize images consistently

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all food items, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -12,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show details of a specific food item """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
