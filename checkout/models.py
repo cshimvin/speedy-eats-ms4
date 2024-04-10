fa38f6e87f0ac6b8c0c8315489c4ece83a38aef2
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-04-10 11:12:56 +0000

    Create basic checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    """ Order summary model """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    """ Order item model """
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
