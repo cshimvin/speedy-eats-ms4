3ed782097bb22136c16a5a57689d01f531544722
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-04-15 15:27:41 +0000

    Add and migrate user profile model

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from products.models import Product
+from profiles.models import UserProfile
 from django.db.models import Sum
 from django.conf import settings
 
@@ -9,6 +10,11 @@ class Order(models.Model):
     """ Order summary model """
     order_number = models.CharField(max_length=32, null=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
+    # Import user profile as a foreign key
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
+
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
