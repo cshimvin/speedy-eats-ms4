460e0240a9f593b5e47bcf2a7ef19738e4a46c78
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-04-16 20:10:26 +0000

    Remove secret key from settings and regenerate key

--- speedy_eats/settings.py
+++ speedy_eats/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-r3=j-uv$dl(7b6ean)cc5p+$4%=6w$=9a^hwtxwaq24s1ez5#g')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
