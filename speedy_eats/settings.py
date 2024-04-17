242ed1c9e4601c08673f4b6f273ab39afe3acae9
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-04-17 23:38:02 +0000

    Change debug variable to check local variables

--- speedy_eats/settings.py
+++ speedy_eats/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'cshimvin-speedy-eats-9486f6bced13.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'cshimvin-speedy-eats-9486f6bced13.herokuapp.com', '8000-cshimvin-speedyeatsms4-9ahjbuhkdms.ws-eu110.gitpod.io']
 
 
 # Application definition
