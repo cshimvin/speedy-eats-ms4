3137be021938ab039a019f017a331b9e81b29f82
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-05-21 09:28:56 +0000

    Add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Review(models.Model):
+    review_title = models.CharField(max_length=254, null=False, blank=False)
+    review_body = models.TextField(null=False, blank=False)
+    rating = models.DecimalField(null=False, blank=False)
+    review_date = models.DateTimeField(auto_now_add=True)
+    reviewer = models.ForeignKey(
+        User, related_name='reviews', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
