1f3d10e777fcdacbc7aafee058af63720598cd2a
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-06-10 15:42:28 +0000

    Add basic templates and views to reviews

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Review(models.Model):
     review_title = models.CharField(max_length=254, null=False, blank=False)
     review_body = models.TextField(null=False, blank=False)
-    rating = models.DecimalField(null=False, blank=False)
+    rating = models.DecimalField(null=False, decimal_places=0, max_digits=1, blank=False)
     review_date = models.DateTimeField(auto_now_add=True)
     reviewer = models.ForeignKey(
         User, related_name='reviews', on_delete=models.CASCADE)
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Review
 
-# Create your views here.
+def add_review(request):
+    """ Display the Add Review form """
+    return render(request, 'reviews/add_review.html')
+
+
+def reviews(request):
+    """ Display the Add Review form """
+    return render(request, 'reviews/reviews.html')
