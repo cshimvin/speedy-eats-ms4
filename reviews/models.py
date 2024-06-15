ad2b2e9a51397ae383718208708cabf0b6b76a07
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-06-15 22:06:51 +0000

    Correct date field in model

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,7 @@ class Review(models.Model):
     review_body = models.TextField(null=False, blank=False)
     rating = models.DecimalField(null=False, decimal_places=0,
                                  max_digits=1, blank=False)
-    review_date = models.DateTimeField(auto_now_add=True),
+    review_date = models.DateField(auto_now_add=True)
     reviewer_name = models.CharField(max_length=254, null=False, blank=False)
 
     def __str__(self):
