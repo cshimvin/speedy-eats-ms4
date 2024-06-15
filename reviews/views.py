bbb50090b04a9da741ab60a858783e36b6babc3e
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-06-15 16:18:25 +0000

    Add login to add review page

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Review
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def add_review(request):
     """ Display the Add Review form """
     return render(request, 'reviews/add_review.html')
