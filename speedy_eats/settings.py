e0f19cffafa5ca21597d249e67fe9f08843d366c
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-06-16 19:45:05 +0000

    Add Driver model

--- speedy_eats/settings.py
+++ speedy_eats/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'profiles',
     'storages',
     'reviews',
+    'staff',
 ]
 
 MIDDLEWARE = [
--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    """
+    Delivery Driver model containing personal details
+    """
+    staff_number = models.CharField(max_length=10, null=False, blank=False)
+    first_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=15, null=False, blank=False)
+    vehicle_type = models.CharField(max_length=254, null=False, blank=False)
+    vehicle_reg_number = models.CharField(max_length=20, null=False,
+                                          blank=False)
+
+    def __str__(self):
+        return self.staff_number
